06e240d1a3eaf359a0c679e56fee348942d47002
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-03-08 15:09:06 +0700

    UPDATE - About Section in About page

--- components/about/AboutMe.vue
+++ components/about/AboutMe.vue
@@ -11,7 +11,7 @@ import { SERVICES } from "~/data/utils";
 
       <div class="mt-14">
         <div class="max-w-5xl mx-auto mt-10">
-          <div class="grid lg:grid-cols-2 gap-8">
+          <div class="grid lg:grid-cols-2 gap-8 items-center">
             <div class="relative">
               <img
                 class="rounded-2xl"
@@ -20,18 +20,47 @@ import { SERVICES } from "~/data/utils";
               />
 
               <div
-                class="absolute bottom-0 -translate-x-[50%] left-[50%] bg-gray-200 px-4 py-2 rounded-t-xl"
+                class="absolute bottom-0 -translate-x-[50%] left-[50%] bg-[#F5F7F9] px-4 py-2 rounded-t-xl"
               >
                 <p
-                  class="uppercase text-lg text-gray-500 product-type font-semibold"
+                  class="uppercase text-lg text-gray-500 about-title font-semibold text-nowrap"
                 >
                   FRONT-END DEVELOPER
                 </p>
               </div>
             </div>
             <div>
-              <h3 class="heading">HELLO EVERYONE</h3>
-              <h4 class="sub-heading mt-3">Hello my name is Rauliqbal</h4>
+              <h3 class="heading !text-left">HELLO EVERYONE</h3>
+              <h4 class="sub-heading mt-3 !text-left">Muhamad Raul Iqbal</h4>
+              <p class="desc mt-8">
+                I am a
+                <span class="text-primary font-medium"
+                  >Front End Developer
+                </span>
+                and <span class="text-primary font-medium">UI Designer,</span> I
+                am used to making projects with
+                <span class="text-emerald-500 font-medium">Vue JS </span>,
+                <span class="text-blue-500 font-medium">React JS</span> and
+                <span class="text-cyan-500 font-medium"> Tailwind CSS, </span> I
+                use <span class="text-gray-800 font-medium">Git </span> as
+                version control and github to store my project source code.
+              </p>
+              <p class="desc mt-6">
+                I am a graduate of
+                <span class="text-primary font-medium"
+                  >Computer Network Engineering Vocational School,</span
+                >
+                have high spirit of motivation in terms of
+                <span class="text-primary font-medium">learning</span>,
+                <span class="text-primary font-medium">seek experience </span>
+                and
+                <span class="text-primary font-medium">seek new things</span>.
+              </p>
+              <p class="desc mt-6">
+                I am able to work in a team well, honestly, hardworking and
+                responsible. Currently looking for a job as Front End Developer
+                in a company.
+              </p>
             </div>
           </div>
         </div>
