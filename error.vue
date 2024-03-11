2515a64312a731784adad413d03746ed8bf9538d
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-03-11 12:56:00 +0700

    FIX - error page

--- error.vue
+++ error.vue
@@ -12,21 +12,27 @@ const handleError = () => clearError({ redirect: "/" });
 </script>
 
 <template>
-  <Navbar />
-  <main id="luxy" class="flex flex-col items-center justify-center py-24">
-    <img
-      class="max-h-[27rem]"
-      src="https://i.pinimg.com/originals/0e/c0/db/0ec0dbf1e9a008acb9955d3246970e15.gif"
-      alt=""
-    />
-    <h2
-      class="text-2xl md:text-3xl leading-relaxed font-semibold text-center mt-4"
-    >
-      {{ error.message }}
-    </h2>
-    <hr />
+  <div>
+    <Navbar />
+    <div id="luxy">
+      <main class="flex flex-col items-center justify-center py-24">
+        <img
+          class="max-h-[27rem]"
+          src="https://i.pinimg.com/originals/0e/c0/db/0ec0dbf1e9a008acb9955d3246970e15.gif"
+          alt=""
+        />
+        <h2
+          class="text-2xl md:text-3xl leading-relaxed font-semibold text-center mt-4"
+        >
+          {{ error.message }}
+        </h2>
+        <hr />
 
-    <button class="btn-primary mt-8" @click="handleError">Back to home</button>
-  </main>
-  <Footer />
+        <button class="btn-primary mt-8" @click="handleError">
+          Back to home
+        </button>
+      </main>
+    </div>
+    <Footer />
+  </div>
 </template>
