8ca7f735fc2ce3c00e52682e175129637ad733a5
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-11-24 21:19:48 +0700

    change dir

--- client/plugins/luxy-scroll.js
+++
@@ -1,8 +0,0 @@
-import Luxy from "luxy.js";
-import anime from "animejs/lib/anime.es.js";
-
-export default ({ app }) => {
-  if (process.client) {
-    Luxy.init();
-  }
-};
--- server/router.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import contactPortfolio from './controllers/portfolio.js';
-
-const router = express.Router();
-
-router.post('/contact', contactPortfolio);
-
-export default router;
